d5360fe41c6db27090bdfce368f4c65c3c083ef4
kimternet <kimternet23@gmail.com>
2024-11-18 00:06:07 +0900

    c++에서의 operators

---
+++ C++ 학습/5_arithmetic_operators.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main() {
+
+    // arithmetic operators = return the result of a specific
+    //                        arithmetic operation(+, -, *, /)
+
+
+    //int students = 20;
+
+    //students = students +1;
+    //students+=1;
+    //students++;
+
+    //students = students - 1;
+    //students-=1;
+    //students--;
+
+    //students = students * 2;
+    //students*=2;
+    
+    //students = students /2;
+    //students/=2;
+    
+    // std::cout << students;
+
+    // int remainder = students %2;
+    // std::cout << remainder;
+
+    // parenthesis
+    // multiplication & division
+    // addition & subtraction
+    
+    int students = 6 -5 + 4 * 3/ 2; 
+    
+    std::cout<<students;
+
+}
