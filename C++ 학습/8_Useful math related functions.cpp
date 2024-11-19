b70a892efb645280c85c093be0eb3f55d4f6e275
kimternet <kimternet23@gmail.com>
2024-11-20 02:03:59 +0900

    c++ math

---
+++ C++ 학습/8_Useful math related functions.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath> // www.cplusplus.com/reference/cmath/
+int main() {
+
+    double x = 3;
+    double y = 4;
+    double i = 3.14;
+    double o = 3.99;
+    double z;
+    double r;
+    double f;
+
+    // z = std::max(x, y);
+    // z = std::min(x, y);
+    // z = pow(2, 4);
+    // z = sqrt(9);
+    // z = abs(-3);
+    // r = round(i);
+    // r = ceil(i);
+    // f = floor(o);
+
+    std::cout<< z << '\n';
+    std::cout<< r << '\n';
+    std::cout<< f << '\n';
+
+}
