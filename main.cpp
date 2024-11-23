b494c682e077f3c3a933a730b66ff1b1b304ec70
김형운(Kim Hyeong_woon) 1989.02.03 <kimternet23@gmail.com>
2024-11-23 13:15:11 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    int num = 20;
+    std::cout << "Hello Wrold" << std::endl;
+    std::cout << "Hello" << "World!" << std::endl;
+    std::cout << num << 'a';
+    std::cout<<num<<' '<<'A'<<std::endl;
+    std::cout << ' ' << 3.14 << std::endl;
+
+    return 0;
+}
